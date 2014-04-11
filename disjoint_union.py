733c59a30dce98457da8b7e6b5aabafaaaed576e
alex <alex.delorenzo@gmail.com>
2014-04-11 13:14:35 -0700

    unions(*args) takes many args

--- disjoint_union.py
+++ disjoint_union.py
@@ -2,113 +2,110 @@ __author__ = 'alex'
 
 
 class DisjointUnion(list):
-	def __init__(self, initial={}):
-		if initial:
-			self |= initial
+    def __init__(self, initial={}):
+        if initial:
+            self |= initial
 
-	def __or__(self, other):
-		try:
-			iterable = set(other)
+    def __or__(self, other):
+        return self.unions(other)
 
-		except TypeError:
-			return self.union(other, other)
+    def __ror__(self, other):
+        return self.__or__(other)
 
-		return self.unions(iterable)
+    def __add__(self, other):
+        return self.__or__(other)
 
-	def __ror__(self, other):
-		return self.__or__(other)
+    def __radd__(self, other):
+        return self.__add__(other)
 
-	def __add__(self, other):
-		return self.__or__(other)
+    def __ior__(self, other):
+        return self.__or__(other)
 
-	def __radd__(self, other):
-		return self.__add__(other)
+    def __iadd__(self, other):
+        return self.__add__(other)
 
-	def __ior__(self, other):
-		return self.__or__(other)
+    def find(self, item):
+        for index, pool in enumerate(self):
+            if item in pool:
+                return index
 
-	def __iadd__(self, other):
-		return self.__add__(other)
+        return None
 
+    def union(self, x, y):
+        xroot, yroot = self.find(x), self.find(y)
 
-	def find(self, item):
-		for index, pool in enumerate(self):
-			if item in pool:
-				return index
+        x_present = xroot is not None
+        y_present = yroot is not None
 
-		return None
+        same_root = xroot == yroot
+        both_present = x_present and y_present
+        one_present = x_present or y_present
 
+        if same_root and both_present:
+            pass
 
-	def union(self, x, y):
-		xroot, yroot = self.find(x), self.find(y)
+        elif both_present:
+            sml, lrg = sorted((xroot, yroot))
 
-		x_present = xroot is not None
-		y_present =	yroot is not None
+            self[sml] |= self[lrg]
+            self.pop(lrg)
 
-		same_root = xroot == yroot
-		both_present = x_present and y_present
-		one_present = x_present or y_present
+        elif one_present:
+            index = xroot if x_present else yroot
+            new_value = y if x in self[index] else x
+            self[index].add(new_value)
 
-		if same_root and both_present:
-			pass
+        else:
+            self.append({x, y})
 
-		elif both_present:
-			sml, lrg = sorted((xroot, yroot))
+        return self
 
-			self[sml] |= self[lrg]
-			self.pop(lrg)
+    def union_iterable(self, iterable):
+        try:
+            iterable = set(iterable)
 
-		elif one_present:
-			index = xroot if x_present else yroot
-			new_value = y if x in self[index] else x
-			self[index].add(new_value)
+        except TypeError as t_e:
+            self.union(iterable, iterable)
+            return self
 
-		else:
-			self.append({x, y})
+        length = len(iterable)
 
-		return self
+        if length == 1:
+            self.append(iterable)
 
+        elif length > 1:
+            initial = iterable.pop()
 
-	def unions(self, set_or_list):
-		try:
-			set_or_list = set(set_or_list)
+            for item in iterable:
+                self.union(initial, item)
 
-		except TypeError as t_e:
-			self.union(set_or_list, set_or_list)
-			return self
+        return self
 
-		length = len(set_or_list)
+    def unions(self, *many_items):
+        if len(many_items) == 1:
+            many_items = many_items[0]
 
-		if length == 1:
-				self.append(set_or_list)
-
-		elif length > 1:
-			initial = set_or_list.pop()
-
-			for item in set_or_list:
-				self.union(initial, item)
-
-		return self
+        return self.union_iterable(set(many_items))
 
 
 def main():
-	a, b, c, d = {1,2,3}, {4,5,6,7,8,9}, {'a', 'b', 0}, None
+    a, b, c, d = {1, 2, 3}, {4, 5, 6, 7, 8, 9}, {'a', 'b', 0}, None
 
-	s = DisjointUnion(a).unions(b).unions(c).union(d, d)
-	print(s)
+    s = DisjointUnion(a).unions(b).unions(c).union(d, d)
+    print(s)
 
-	s.unions({0, 1, 4})
-	print(s)
+    s.unions({0, 1, 4})
+    print(s)
 
-	x, y, z = ValueError, 'lol if youre reading this', {x for x in range(90, 80, -1)}
+    x, y, z = ValueError, 'lol if youre reading this', {x for x in range(90, 80, -1)}
 
-	s += x
-	s |= {'four', 'score'}
-	print(s)
+    s += x
+    s |= {'four', 'score'}
+    print(s)
 
-	('!@#$' + (s + x) + y) | ( {22,44} | z)
-	print(s)
+    ('!@#$' + (s + x) + y) | ( {22, 44} | z)
+    print(s)
 
 
 if __name__ == "__main__":
-	main()
+    main()
