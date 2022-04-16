c666ea82e448ec43d08a2ace38c9e428469e9adb
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2022-04-16 10:02:31 +0000

    Use `collections.abc` instead of just `collections`

--- disjoint_union/disjoint_union.py
+++ disjoint_union/disjoint_union.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Optional, Union, Any, Hashable, \
   Set, Tuple
-from collections import Iterable, Sequence
+from collections.abc import Iterable, Sequence
 
 
 Iterables = Union[Iterable, Sequence]
